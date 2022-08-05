754ad4e2546e57bed0bef449f6d508ec9e9013b6
Antoine Greuzard <antoin.greuzard@gmail.com>
2022-08-05 09:05:57 +0200

    disable register and password change

--- routes/auth.php
+++ routes/auth.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function () {
     /*Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
-	
+
     Route::post('register', [RegisteredUserController::class, 'store']);*/
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
@@ -25,13 +25,13 @@ Route::middleware('guest')->group(function () {
                 ->name('password.request');
 
     Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-                ->name('password.email');*/
+                ->name('password.email');
 
     Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
                 ->name('password.reset');
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
-                ->name('password.update');
+                ->name('password.update');*/
 });
 
 Route::middleware('auth')->group(function () {
