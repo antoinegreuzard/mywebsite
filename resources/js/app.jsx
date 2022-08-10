a0df711d22db71687a118e74e3d66ffe2b9e6ac5
Antoine Greuzard <antoin.greuzard@gmail.com>
2022-08-10 12:58:40 +0200

    optimize app

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { render } from 'react-dom';
-import { createInertiaApp } from '@inertiajs/inertia-react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import {render} from 'react-dom';
+import {createInertiaApp} from '@inertiajs/inertia-react';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
 import '../css/app.scss';
 import "aos/dist/aos.css";
 
 createInertiaApp({
-	id: 'app',
+    id: 'app',
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
-    setup({ el, App, props }) {
+    setup({el, App, props}) {
         return render(<App {...props} />, el);
     },
 });
