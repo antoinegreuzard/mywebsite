da627e68bcc2e9aaaabb5b5094b9fde75196a1ff
Antoine Greuzard <antoin.greuzard@gmail.com>
2022-08-10 12:56:53 +0200

    added github link

--- resources/js/Components/Social.jsx
+++ resources/js/Components/Social.jsx
@@ -1,26 +1,30 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faLinkedinIn, faGithub} from '@fortawesome/free-brands-svg-icons'
 
 const SocialShare = [
-  {
-    iconName: faLinkedinIn,
-    link: "https://fr.linkedin.com/in/antoine-greuzard",
-  }
+    {
+        iconName: faLinkedinIn,
+        link: "https://fr.linkedin.com/in/antoine-greuzard",
+    },
+    {
+        iconName: faGithub,
+        link: "https://github.com/antoinegreuzard",
+    }
 ];
 
 const Social = () => {
-  return (
-    <ul className="social list-unstyled pt-1 mb-5">
-      {SocialShare.map((val, i) => (
-        <li key={i}>
-          <a href={val.link} target="_blank" rel="noreferrer">
-            <i><FontAwesomeIcon icon={val.iconName} /></i>
-          </a>
-        </li>
-      ))}
-    </ul>
-  );
+    return (
+        <ul className="social list-unstyled pt-1 mb-5">
+            {SocialShare.map((val, i) => (
+                <li key={i}>
+                    <a href={val.link} target="_blank" rel="noreferrer">
+                        <i><FontAwesomeIcon icon={val.iconName}/></i>
+                    </a>
+                </li>
+            ))}
+        </ul>
+    );
 };
 
 export default Social;
